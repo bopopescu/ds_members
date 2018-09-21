eb421bccc99050958941cc5ff45d7641898f95a3
EmmanueleSalvati <emmanuele.salvati@gmail.com>
2018-09-21 17:23:36 -0400

    add features in census

--- view_census.py
+++ view_census.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import psycopg2
+from sqlalchemy import create_engine
 pd.set_option('max_colwidth', 200)
 
 # stuff taken from https://www.earthdatascience.org/tutorials/get-cenus-data-with-cenpy/
@@ -18,10 +20,19 @@ pd.set_option('max_colwidth', 200)
 
 # con = cen.base.Connection(dataset)
 
+localdb = create_engine(
+    'postgresql://',
+    echo=False,
+    pool_recycle=300,
+    creator=lambda _: psycopg2.connect(service="rockets-local"))
+
 # Load population file
 pop = pd.read_csv(
     "~/Data/ACS_16_5YR_S1101/ACS_16_5YR_S1101_with_ann.csv",
-    usecols=['GEO.id2', 'HC01_EST_VC02', 'HC02_EST_VC02', 'HC01_EST_VC06', 'HC01_EST_VC10'],
+    usecols=[
+        'GEO.id2', 'HC01_EST_VC02', 'HC02_EST_VC02', 'HC01_EST_VC06',
+        'HC01_EST_VC10'
+    ],
     skiprows=[1],
     dtype={
         'GEO.id2': 'str',
@@ -82,3 +93,72 @@ df.replace({'2,500-': '-999', '250,000+': '999999'}, inplace=True)
 
 df['med_hh_income'] = df['med_hh_income'].astype('float')
 df['med_fam_income'] = df['med_fam_income'].astype('float')
+
+n_kids = pd.read_csv(
+    "~/Data/ACS_16_5YR_B09001/ACS_16_5YR_B09001_with_ann.csv",
+    usecols=['GEO.id2', 'HD01_VD01'],
+    skiprows=[1],
+    dtype={
+        'GEO.id2': 'str',
+        'HD01-VD01': 'int'
+    })
+n_kids.columns = ['zip', 'n_kids']
+
+sch = pd.read_csv(
+    "~/Data/ACS_16_5YR_S1401/ACS_16_5YR_S1401_with_ann.csv",
+    usecols=[
+        'GEO.id2', 'HC01_EST_VC03', 'HC02_EST_VC03', 'HC04_EST_VC03',
+        'HC06_EST_VC03'
+    ],
+    skiprows=[1],
+    na_values=['-'],
+    dtype={
+        'GEO.id2': 'str',
+        'HC01_EST_VC03': 'int',
+        'HC02_EST_VC03': 'float',
+        'HC04_EST_VC03': 'float',
+        'HC06_EST_VC03': 'float'
+    })
+sch.columns = [
+    'zip', 'kids_school', 'kids_school_perc', 'kids_publ_school_perc',
+    'kids_priv_school_perc'
+]
+
+sch.fillna(0, inplace=True)
+
+df = pd.merge(df, n_kids)
+df = pd.merge(df, sch)
+
+hh = pd.read_csv(
+    "~/Data/ACS_16_5YR_DP04/ACS_16_5YR_DP04_with_ann.csv",
+    usecols=[
+        'GEO.id2', 'HC03_VC160', 'HC03_VC161', 'HC03_VC162', 'HC03_VC163',
+        'HC03_VC164', 'HC03_VC199', 'HC03_VC200', 'HC03_VC201', 'HC03_VC202',
+        'HC03_VC203', 'HC03_VC204'
+    ],
+    skiprows=[1],
+    na_values=['-'],
+    dtype={
+        'GEO.id2': 'str',
+        'HC03_VC160': 'float',
+        'HC03_VC161': 'float',
+        'HC03_VC162': 'float',
+        'HC03_VC163': 'float',
+        'HC03_VC164': 'float',
+        'HC03_VC199': 'float',
+        'HC03_VC200': 'float',
+        'HC03_VC201': 'float',
+        'HC03_VC202': 'float',
+        'HC03_VC203': 'float',
+        'HC03_VC204': 'float'
+    })
+
+hh.columns = [
+    'zip', 'smocapi_20', 'smocapi_25', 'smocapi_30', 'smocapi_35',
+    'smocapi_high', 'grapi_15', 'grapi_20', 'grapi_25', 'grapi_30', 'grapi_35',
+    'grapi_high'
+]
+hh.fillna(0, inplace=True)
+df = pd.merge(df, hh)
+
+df.to_sql("census", localdb, schema='dwh', if_exists='replace', index=False)
