5b10dc7e733d23547e687786d8e77f377e9d88cd
EmmanueleSalvati <emmanuele.salvati@gmail.com>
2019-09-05 14:27:38 -0400

    add prophet

--- backpacks.py
+++ backpacks.py
@@ -6,6 +6,9 @@ from matplotlib import pyplot as plt
 import seaborn as sns
 sns.set_style('whitegrid')
 
+import plotly
+from fbprophet import Prophet
+
 REDSHIFT = create_engine(
         'postgresql://',
         echo=True,
@@ -18,7 +21,7 @@ REDSHIFT = create_engine(
 def mock_df(ts, start, end):
     dfs = []
 
-    for cw in ts['cw'].unique():
+    for cw in ts['style_number'].unique():
 
 
 
@@ -30,21 +33,21 @@ def mock_df(ts, start, end):
 
 
 def complete_time_series(ts):
-        """[summary]
+    """[summary]
         
         Arguments:
             ts {[type]} -- [description]
         """
-        z = mock_df(
-                ts,
-                pd.to_datetime(ts['shipped_on']).min(),
-                pd.to_datetime(ts['shipped_on']).max()
-        )
-        g = pd.merge(z, ts, how='left')
-        g.fillna(0, inplace=True)
-        g.drop('zeros', axis=1, inplace=True)
+    z = mock_df(
+            ts,
+            pd.to_datetime(ts['shipped_on']).min(),
+            pd.to_datetime(ts['shipped_on']).max()
+    )
+    g = pd.merge(z, ts, how='left')
+    g.fillna(0, inplace=True)
+    g.drop('zeros', axis=1, inplace=True)
 
-        return g
+    return g
 
 
 
@@ -111,11 +114,22 @@ ts['shipped_on'] = pd.to_datetime(ts['shipped_on'])
 # r = complete_time_series(rainbow)
 # r['cumsum'] = r['sold'].cumsum()
 
-# bomber = ts.loc[ts['cw']=='2010-025', ]
-# b = complete_time_series(bomber)
-# b['cumsum'] = b['sold'].cumsum()
-
 bomber = ts.loc[ts['style_number'].str.contains('2010'), ]
+b = complete_time_series(bomber)
+b['cumsum'] = b['sold'].cumsum()
+
+train = b.loc[b['shipped_on'] < '2018-09-01', ['shipped_on', 'cumsum']]
+train.rename(columns={'shipped_on': 'ds', 'cumsum': 'y'}, inplace=True)
+
+
+m = Prophet()
+m.fit(train)
+future = m.make_future_dataframe(periods=365)
+forecast = m.predict(future)
+
+forecast[['ds', 'yhat', 'yhat_lower', 'yhat_upper']].tail()
+fig1 = m.plot(forecast)
+fig2 = m.plot_components(forecast)
 
 
 def ts_plot(data, var='cumsum', title=None, hue=None, palette=None, style=None):
@@ -140,5 +154,3 @@ def ts_plot(data, var='cumsum', title=None, hue=None, palette=None, style=None):
     ax.set_title(title)
 
     # return fig
-
-
